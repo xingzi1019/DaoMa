ed09f4fe3fe79b3a341f84aa945f9466ff2137ee
xingzi1019 <xingzi1019@qq.com>
2025-12-08 01:42:42 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,83 +1,75 @@
-#define  _CRT_SECURE_NO_WARNINGS
-#include "game.h"
-#include <stdio.h>
-void menu()//打印菜单
+#define _CRT_SECURE_NO_WARNINGS 1
+//需求如下
+//实现一个静态通讯录:
+//人的信息:
+//名字+年龄+性别+电话+地址
+//其他功能:
+//1.存放100个人的信息
+//2.增加联系人
+//3.删除指定联系人
+//4.查找联系人
+//5.修改联系人
+//6.排序
+//7.显示联系人
+//一共有3个版本 此为一版本
+//1.静态的版本
+//2.动态的版本
+//3.文件的版本
+#include "Contact.h"//包含自定义的头文件
+void menu()
 {
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@             @@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@    1.开始   @@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@    0.退出   @@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@             @@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-}
-
-void game()
-{
-	char ret = 0;
-	char board[ROW][COL] = { 0 };
-	//初始化棋盘的函数
-	InitBoard(board, ROW, COL);
-	DisPlayBoard(board, ROW, COL);
-	//下棋
-	while (1)//0为假非0为真
-	{
-		PlayerMove(board, ROW, COL);
-		//胜利判断
-		ret = IsWin(board, ROW, COL);
-		if (ret != 'C')
-		{
-			break;
-		}
-		DisPlayBoard(board, ROW, COL);
-		ComputerMove(board, ROW, COL);
-		//胜利判断
-		ret = IsWin(board, ROW, COL);
-		if (ret != 'C')
-		{
-			break;
-		}
-		DisPlayBoard(board, ROW, COL);
-	}
-	if (ret == '*')
-	{
-		printf("玩家获胜\n");
-	}
-	else if (ret == '#')
-	{
-		printf("人机获胜\n");
-	}
-	else
-	{
-		printf("平局\n");
-	}
-	DisPlayBoard(board, ROW, COL);
+	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	printf("@@@@@@@                           @@@@@@\n");
+	printf("@@@@@@@    1.Add       2.Del      @@@@@@\n");
+	printf("@@@@@@@    3.Search    4.Modify   @@@@@@\n");
+	printf("@@@@@@@    5.show      6.Sort     @@@@@@\n");
+	printf("@@@@@@@           0.Exit          @@@@@@\n");
+	printf("@@@@@@@                           @@@@@@\n");
+	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
 }
 int main()
 {
-	srand((unsigned int)time(NULL));//设置随机数的生成起点
 	int input = 0;
+	Contact con;//通讯录
+	//初始化通讯录
+	InitContact(&con);
 	do
 	{
-		menu();//打印菜单
-		printf("请输入选择（1或者0）:>");
+		menu();
+		printf("请选择:");
 		scanf("%d", &input);
 		switch (input)
 		{
 		case 1:
-			printf("\n开始三子棋游戏\n");
-			game();
+			AddContact(&con);
+			break;
+		case 2:
+			DelContact(&con);
+			break;
+		case 3:
+			SearchContact(&con);
+			break;
+		case 4:
+			ModifyContact(&con);
+			break;
+		case 5:
+			ShowContact(&con);
+			break;
+		case 6:
+			SortContact(&con);
 			break;
 		case 0:
-			printf("\n退出游戏\n");
+			printf("退出通讯录\n");
 			break;
 		default:
-			printf("\n选择错误\n请重新选择\n");
-			break;
+			printf("选择错误请重新选择\n");
 		}
 	} while (input);
 	return 0;
 }
+
+//排序可以按照意愿来 可以是 名字 年龄 电话等
+//清空屏幕看着清爽
+//一次性多修改几个联系人的信息
